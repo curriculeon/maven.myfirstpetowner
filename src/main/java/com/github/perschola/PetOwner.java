ad495f82181c504088eb7f977c3d909e72ce1152
Mohammed N <mnaser11218@gmail.com>
2024-10-26 11:59:13 -0400

    updated petowner class

--- src/main/java/com/github/perschola/PetOwner.java
+++ src/main/java/com/github/perschola/PetOwner.java
@@ -4,9 +4,11 @@ package com.github.perschola;
  * Created by leon on 12/17/2019.
  */
 public class PetOwner {
+    private Pet pet=null;
     public void setPet(Pet pet) {
+        this.pet = pet;
     }
     public Pet getPet() {
-        return null;
+        return pet;
     }
 }
